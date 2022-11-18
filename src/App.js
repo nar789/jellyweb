83c8078f9f7f45c018837cd4b719cb4977176071
nar789 <admin@hume.co.kr>
2022-11-18 09:05:11 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import { Canvas } from '@react-three/fiber';
+import { Box, OrbitControls } from '@react-three/drei';
+import { Happy } from './Happy';
 import './App.css';
+import { Sad } from './Sad';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas>
+      <OrbitControls />
+      <directionalLight intensity={0.6} />
+      <ambientLight intensity={3} />
+      <Happy />
+    </Canvas>
   );
 }
 
